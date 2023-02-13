c24367da4edacd6b0c84a8b0c67c75be9f83818d
clean17 <piw940317@gmail.com>
2023-02-13 12:04:18 +0900

    feat: jdbc 연결까지만

---
+++ app/src/main/java/myapp/config/DB.java
@@ -0,0 +1,25 @@
+package myapp.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DB {
+    
+    public static Connection getConnection(){
+        // Connection conn = null;
+		// Statement stmt = null;
+		// ResultSet rs;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/metadb","root","1234");
+            System.out.println("DB 연결성공");
+            return conn;
+        } catch (Exception e) {
+            e.printStackTrace();;
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        getConnection();
+    }
+}
